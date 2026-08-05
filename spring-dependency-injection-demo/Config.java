5bce58fed19560dab0f394f2aca5b46dbae292fe
Yokesh S <yokeshsampath25@gmail.com>
2026-08-05 18:37:59 +0530

    Add Spring configuration class for component scanning

---
+++ spring-dependency-injection-demo/Config.java
@@ -0,0 +1,10 @@
+package pack;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages ="pack")
+public class Config {
+
+}
