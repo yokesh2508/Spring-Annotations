150162f91c071ae7cb5a1b84e886bc737a70df09
Yokesh S <yokeshsampath25@gmail.com>
2026-08-05 18:26:18 +0530

    Add BankConfig class for Spring configuration

---
+++ spring-has-a-relationship/BankConfig.java
@@ -0,0 +1,10 @@
+package has;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages ="has" )
+public class BankConfig {
+
+}
