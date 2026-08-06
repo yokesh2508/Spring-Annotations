da1ccf211555005831eb12c00c31f6cc3e1d3a21
Yokesh S <yokeshsampath25@gmail.com>
2026-08-06 16:47:09 +0530

    Add Main class for Spring application context

---
+++ spring-collection-injection-demo/list/Main.java
@@ -0,0 +1,16 @@
+package list;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Main {
+
+	public static void main(String[] args) {
+		ApplicationContext context = new AnnotationConfigApplicationContext(ListConfig.class);
+
+		Movie m = (Movie) context.getBean("m");
+
+		System.out.println(m);
+	}
+
+}
