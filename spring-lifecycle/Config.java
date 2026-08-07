431f0eaa2e5c6bdd7b6a017e914df3ba44208553
Yokesh S <yokeshsampath25@gmail.com>
2026-08-07 22:56:31 +0530

    Add Spring configuration class for component scanning

---
+++ spring-lifecycle/Config.java
@@ -0,0 +1,10 @@
+package lifeCycle;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan
+public class Config {
+
+}
